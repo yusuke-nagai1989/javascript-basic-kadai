731ea08da9cc660ad89e8a448abd1d873b543a93
yusuke-nagai1989 <156492699+yusuke-nagai1989@users.noreply.github.com>
2024-02-20 00:09:37 +0900

    kadai_mdn_jsのappuro-do

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const date = new Date();
+
+const year =  date.getFullYear() ;
+const month =  date.getMonth() + 1;
+const day =  date.getDate();
+
+
+console.log(year + '年' + month + '月' + day + '日')
