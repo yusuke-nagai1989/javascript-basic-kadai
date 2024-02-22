6f8241eb4e3e6700897f268569c0310dcf214a66
yusuke-nagai1989 <156492699+yusuke-nagai1989@users.noreply.github.com>
2024-02-22 22:24:29 +0900

    課題021のアップロードです

---
+++ kadai_021/event.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('btn')
+const text = document.getElementById('text')
+
+
+btn.addEventListener('click', () => {
+  
+  setTimeout(() => {
+
+  text.textContent='';
+  
+  const h2 = document.createElement('h2');
+  h2.textContent = 'ボタンをクリックしました';
+  document.querySelector('body').prepend(h2);
+  
+  },2000);
+})
